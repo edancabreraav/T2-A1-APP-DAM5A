b22c2f0919e3e03d13671236a4768284e704df41
cabrera <edancabreraav@ittepic.edu.mx>
2024-09-20 21:33:25 -0600

    Haste el 4.5

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,104 @@
-import { React, useReducer } from 'react'
+import { React, useEffect, useReducer } from 'react';
+
+const initialState = {
+    validando: true,
+    token: null,
+    username: '',
+    nombre: ''
+};
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'logout':
+            return {
+                token: null,
+                username: '',
+                nombre: '',
+                validando: false,
+            };
+        case 'login':
+            const { nombre, username } = action.payload;
+            return {
+                validando: false,
+                token: 'TOKEN123',
+                username,
+                nombre,
+            };
+        default:
+            return state;
+    }
+};
 
 export const Login = () => {
-    const initialState = {
-        validando: true,
-        token: null,
-        username: '',
-        nombre: ''
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch({ type: 'logout' });
+        }, 2500);
+    }, []);
+
+    const login = () => {
+        dispatch({
+            type: 'login',
+            payload: {
+                username: 'cabrera',
+                nombre: 'Edson'
+            }
+        });
+    };
+
+    const logout = () => {
+        dispatch({ type: 'logout' });
+    };
+
+    if (state.validando) {
+        return (
+            <>
+                <h3>Login</h3>
+                <div className="InfoAlert">
+                    Validando Información...
+                </div>
+            </>
+        );
     }
-    
-    
 
     return (
         <>
-            <h2>Login</h2>
-            <div className="AlertInfo_Div">
-                Validando Información...
-            </div>
-            <div className="AlertDanger_Div">
-                No Autenticado...
-            </div>
-            <div className="AlertSuccess_Div">
-                Autenticado...
-            </div>
-            <button
-                className="Login_Button"
-            >
-                Login
-            </button>
-            <button
-                className="Logout_Button"
-            >
-                Logout
-            </button>
+            <h3>Login</h3>
+            {
+                (state.token)
+                    ? (
+                        <div className="SuccessAlert">
+                            Autenticado como: {state.nombre}
+                        </div>
+                    )
+                    : (
+                        <div className="DangerAlert">
+                            No Autenticado...
+                        </div>
+                    )
+            }
+            {
+                (state.token)
+                    ? (
+                        <button
+                            className="Logout_Button"
+                            onClick={logout}
+                        >
+                            Logout
+                        </button>
+                    )
+                    : (
+                        <button
+                            className="Login_Button"
+                            onClick={login}
+                        >
+                            Login
+                        </button>
+                    )
+            }
         </>
-    )
-}
+    );
+};
+
